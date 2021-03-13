23c217709b6e0343f3620003ec40ff65e45bbde3
hiago <hiagohps360@gmail.com>
2021-03-12 22:57:48 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => (
+  <View>
+    <Text>Hello</Text>
+  </View>
+);
+
+export default App;
