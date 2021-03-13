87a761e24aa759fb2311bf5605071beb5a7ae067
hiago <hiagohps360@gmail.com>
2021-03-12 23:16:42 -0300

    feat: config navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
 import { View, Text, StatusBar } from 'react-native';
-
-// import { Container } from './styles';
+import { NavigationContainer } from '@react-navigation/native';
+import AuthRoutes from './routes';
 
 const App: React.FC = () => {
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
-        <Text>Hello</Text>
-      </View>
+      <NavigationContainer>
+        <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+        <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+          <AuthRoutes />
+        </View>
+      </NavigationContainer>
     </>
   );
 };
